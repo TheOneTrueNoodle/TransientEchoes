5046d1621bd3020373c6f86acef4dcbdcaf9e5d9
TheOneTrueNoodle <72693746+TheOneTrueNoodle@users.noreply.github.com>
2022-10-26 14:57:46 +0100

    Working on opening up the inventory

--- Transient_Echoes_Project/Assets/Scripts/Pedestal.cs
+++ Transient_Echoes_Project/Assets/Scripts/Pedestal.cs
@@ -7,18 +7,73 @@ public class Pedestal : MonoBehaviour
 {
     [SerializeField] private Canvas InventoryCanvas;
     public Camera Player1cam;
+    private PlayerMovement Player1;
     public Camera Player2cam;
+    private PlayerMovement Player2;
+    private Camera usingCam;
+
+    private PlayerMovement playerWithInventoryOpen;
 
     private bool InventoryActive = false;
+    [SerializeField] private GameObject OpenInventoryPrompt;
 
-    public void Update()
+    private void Start()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && InventoryActive == false)
+        PlayerMovement[] players = FindObjectsOfType<PlayerMovement>();
+        for(int i = 0; i < players.Length; i++)
         {
-
+            if(players[i].PlayerNum == PlayerNum.Player1)
+            {
+                Player1 = players[i];
+            }
+            else
+            {
+                Player2 = players[i];
+            }
         }
-
-
     }
 
+    private void Update()
+    {
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        OpenInventoryPrompt.SetActive(true);
+        if (collision.GetComponent<PlayerMovement>() && Input.GetKeyDown(KeyCode.Space))
+        {
+            PlayerMovement pMovement = collision.GetComponent<PlayerMovement>();
+            if(InventoryActive == false)
+            {
+                if (pMovement.PlayerNum == PlayerNum.Player1)
+                {
+                    usingCam = Player1cam;
+                    Player1.CanMove = false;
+                    playerWithInventoryOpen = Player1;
+                }
+                else
+                {
+                    usingCam = Player2cam;
+                    Player2.CanMove = false;
+                    playerWithInventoryOpen = Player2;
+                }
+
+                InventoryCanvas.gameObject.SetActive(true);
+                InventoryCanvas.worldCamera = usingCam;
+            }
+            else if(InventoryActive == true)
+            {
+                playerWithInventoryOpen.CanMove = true;
+                InventoryCanvas.gameObject.SetActive(false);
+            }
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        OpenInventoryPrompt.SetActive(true);
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        OpenInventoryPrompt.SetActive(false);
+    }
 }
