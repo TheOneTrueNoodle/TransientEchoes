e2ed909aefefd15d739bfde8c1785e344934ac57
TheOneTrueNoodle <72693746+TheOneTrueNoodle@users.noreply.github.com>
2022-11-02 15:09:57 +0000

    Some improvements to the button

--- Transient_Echoes_Project/Assets/Scripts/Button.cs
+++ Transient_Echoes_Project/Assets/Scripts/Button.cs
@@ -6,12 +6,18 @@ using UnityEngine;
 public class Button : MonoBehaviour
 {
     public GameObject Bridge;
+    public SpriteRenderer buttonSprite;
+    public Color buttonColor;
+    public Color buttonPressedColor;
+
     private void OnTriggerEnter2D(Collider2D other)
     {
-        Bridge.SetActive(true); 
+        Bridge.SetActive(true);
+        buttonSprite.color = buttonPressedColor;
     }
     private void OnTriggerExit2D(Collider2D other)
     {
         Bridge.SetActive(false);
+        buttonSprite.color = buttonColor;
     }
 }
